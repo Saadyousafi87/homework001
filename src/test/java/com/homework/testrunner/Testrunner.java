9fd6b8bc8c8901d622a40111ddcc5622c4362199
saadyousafi87 <saad.yousafi87@gmail.com>
2017-11-26 10:01:27 -0500

    added packages and codes

---
+++ src/test/java/com/homework/testrunner/Testrunner.java
@@ -0,0 +1,12 @@
+package com.homework.testrunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		features = {"features"}, glue = {"com.homework.stepdefs"}
+		)
+
+public class Testrunner extends AbstractTestNGCucumberTests {
+
+}
